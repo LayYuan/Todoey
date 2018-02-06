b6551ec1df1d0072e7d60cf6aa13ff908fdf4720
Yuan25 <chuahlayyuan@gmail.com>
2018-02-06 11:42:04 +0800

    convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by LayYuan on 05/02/2018.
+//  Copyright © 2018 Yuan25. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
